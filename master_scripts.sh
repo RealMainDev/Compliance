485c18e6000e4972c3cc6b50a0605790841d1788
CloudGarageAdmin <33375644+BetterWorldCloud@users.noreply.github.com>
2018-06-12 16:51:46 -0500

    Update master_scripts.sh

--- master_scripts.sh
+++ master_scripts.sh
@@ -4,7 +4,7 @@ bldylw='\033[1;33m'
 txtrst='\033[0m'
 
 timeStamp() {
-  printf "%b\n" "${bldylw}$1$2${txtrst}\n
+  printf "%b\n" "${bldylw}$1${txtrst}\n
 }
 
 myEcho () {
@@ -21,7 +21,7 @@ myEcho "# ----------------------------------------------------------------------
 Start_Time=`date`
 Yell="COMPLIANCE TIMESTAMP IS: "
 
-timeStamp Yell Start_Time
+timeStamp "$Yell"
 
 # Program requirement check
 requirements='grep pgrep python'
